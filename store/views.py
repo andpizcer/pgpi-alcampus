50f07a1ebf97c472fbf44efea148e139c7918ee2
Victoria Ruiz Delgado <ruizvictoria10@gmail.com>
2024-11-19 21:20:44 +0100

    feat: Control de accesos y permisos

--- store/views.py
+++ store/views.py
@@ -10,9 +10,20 @@ from django.contrib import messages
 from orders.models import OrderProduct
 from django.shortcuts import render
 from .models import Product
+from django.contrib.auth.decorators import login_required, user_passes_test
+
 
 
 # Create your views here.
+def is_admin(user):
+    return user.is_staff
+
+@login_required
+@user_passes_test(is_admin)
+def admin_dashboard(request):
+    # Código para el dashboard del administrador
+    return render(request, 'admin_dashboard.html')
+
 def store(request, category_slug=None):
     categories = None
     products = None
