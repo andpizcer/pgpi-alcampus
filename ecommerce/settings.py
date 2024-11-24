1434078405b8c92474f932241abeb6bd87e18be6
Andrés Pizzano <andpizcer@alum.us.es>
2024-11-24 20:40:40 +0100

    feat: Enviar correos al completar compras

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,78 +21,78 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'q6(e+9d!-ea38h%mja2&zv!_!tme!2s5ajsstg9^6bx@p4w-8x'
+SECRET_KEY = "q6(e+9d!-ea38h%mja2&zv!_!tme!2s5ajsstg9^6bx@p4w-8x"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'category',
-    'accounts',
-    'store',
-    'carts',
-    'orders',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "category",
+    "accounts",
+    "store",
+    "carts",
+    "orders",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django_session_timeout.middleware.SessionTimeoutMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "django_session_timeout.middleware.SessionTimeoutMiddleware",
 ]
 
 
 SESSION_EXPIRE_SECONDS = 1800
 SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
-SESSION_TIMEOUT_REDIRECT = 'accounts/login'
+SESSION_TIMEOUT_REDIRECT = "accounts/login"
 
-ROOT_URLCONF = 'ecommerce.urls'
+ROOT_URLCONF = "ecommerce.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
-                'category.context_processors.menu_links',
-                'carts.context_processors.counter',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": ["templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+                "category.context_processors.menu_links",
+                "carts.context_processors.counter",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'ecommerce.wsgi.application'
+WSGI_APPLICATION = "ecommerce.wsgi.application"
 
-AUTH_USER_MODEL = 'accounts.Account'
+AUTH_USER_MODEL = "accounts.Account"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -102,16 +102,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -119,9 +119,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -131,25 +131,32 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'static'
-STATICFILES_DIRS = [
-    'ecommerce/static'
-]
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "static"
+STATICFILES_DIRS = ["ecommerce/static"]
 
-MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR /'media'
-
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 
 from django.contrib.messages import constants as messages
-MESSAGE_TAGS = {
-    messages.ERROR : 'danger',
-}
 
+MESSAGE_TAGS = {
+    messages.ERROR: "danger",
+}
 
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+### CREACIÓN DE SMTP DE CORREO
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.sendgrid.net"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = "apikey"  # Este es un valor fijo de SendGrid
+EMAIL_HOST_PASSWORD = "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"  # Tu clave de API generada
+EMAIL_FROM = "gestor.alcampus@gmail.com"
--- orders/views.py
+++ orders/views.py
@@ -12,14 +12,16 @@ from django.template.loader import render_to_string
 
 def payments(request):
     body = json.loads(request.body)
-    order = Order.objects.get(user=request.user, is_ordered=False, order_number=body['orderID'])
+    order = Order.objects.get(
+        user=request.user, is_ordered=False, order_number=body["orderID"]
+    )
 
     payment = Payment(
-        user = request.user,
-        payment_id = body['transID'],
-        payment_method = body['payment_method'],
-        amount_id = order.order_total,
-        status = body['status'],
+        user=request.user,
+        payment_id=body["transID"],
+        payment_method=body["payment_method"],
+        amount_id=order.order_total,
+        status=body["status"],
     )
 
     payment.save()
@@ -52,21 +54,32 @@ def payments(request):
 
     CartItem.objects.filter(user=request.user).delete()
 
-    mail_subject = 'Tu compra fue realizada!'
-    body = render_to_string('orders/order_recieved_email.html', {
-        'user': request.user,
-        'order': order,
-    })
+    ordered_products = OrderProduct.objects.filter(order_id=order.id)
+
+    mail_subject = "Tu compra fue realizada!"
+    body = render_to_string(
+        "orders/order_recieved_email.html",
+        {
+            "order": order,
+            "ordered_products": ordered_products,
+            "order_number": order.order_number,
+            "transID": payment.payment_id,
+            "payment": payment,
+            "subtotal": order.order_total - order.tax,
+        },
+    )
 
     to_email = request.user.email
-    send_email = EmailMessage(mail_subject, body, to=[to_email])
+    send_email = EmailMessage(
+        mail_subject, body, to=[to_email], from_email="gestor.alcampus@gmail.com"
+    )
+    send_email.content_subtype = "html"
     send_email.send()
 
     data = {
-        'order_number': order.order_number,
-        'transID': payment.payment_id,
+        "order_number": order.order_number,
+        "transID": payment.payment_id,
     }
-
 
     return JsonResponse(data)
 
@@ -78,68 +91,68 @@ def place_order(request, total=0, quantity=0):
     cart_count = cart_items.count()
 
     if cart_count <= 0:
-        return redirect('store')
+        return redirect("store")
 
     grand_total = 0
     tax = 0
 
     for cart_item in cart_items:
-        total += (cart_item.product.price * cart_item.quantity)
+        total += cart_item.product.price * cart_item.quantity
         quantity += cart_item.quantity
 
-    tax = round((16/100) * total, 2)
+    tax = round((16 / 100) * total, 2)
     grand_total = total + tax
 
-
-    if request.method == 'POST':
+    if request.method == "POST":
         form = OrderForm(request.POST)
 
         if form.is_valid():
             data = Order()
             data.user = current_user
-            data.first_name = form.cleaned_data['first_name']
-            data.last_name = form.cleaned_data['last_name']
-            data.phone = form.cleaned_data['phone']
-            data.email = form.cleaned_data['email']
-            data.address_line_1 = form.cleaned_data['address_line_1']
-            data.address_line_2 = form.cleaned_data['address_line_2']
-            data.country = form.cleaned_data['country']
-            data.city = form.cleaned_data['city']
-            data.state = form.cleaned_data['state']
-            data.order_note = form.cleaned_data['order_note']
+            data.first_name = form.cleaned_data["first_name"]
+            data.last_name = form.cleaned_data["last_name"]
+            data.phone = form.cleaned_data["phone"]
+            data.email = form.cleaned_data["email"]
+            data.address_line_1 = form.cleaned_data["address_line_1"]
+            data.address_line_2 = form.cleaned_data["address_line_2"]
+            data.country = form.cleaned_data["country"]
+            data.city = form.cleaned_data["city"]
+            data.state = form.cleaned_data["state"]
+            data.order_note = form.cleaned_data["order_note"]
             data.order_total = grand_total
             data.tax = tax
-            data.ip = request.META.get('REMOTE_ADDR')
+            data.ip = request.META.get("REMOTE_ADDR")
             data.save()
 
-            yr=int(datetime.date.today().strftime('%Y'))
-            mt=int(datetime.date.today().strftime('%m'))
-            dt=int(datetime.date.today().strftime('%d'))
-            d = datetime.date(yr,mt,dt)
+            yr = int(datetime.date.today().strftime("%Y"))
+            mt = int(datetime.date.today().strftime("%m"))
+            dt = int(datetime.date.today().strftime("%d"))
+            d = datetime.date(yr, mt, dt)
             current_date = d.strftime("%Y%m%d")
             order_number = current_date + str(data.id)
             data.order_number = order_number
             data.save()
 
-            order = Order.objects.get(user=current_user, is_ordered=False, order_number=order_number)
+            order = Order.objects.get(
+                user=current_user, is_ordered=False, order_number=order_number
+            )
             context = {
-                'order': order,
-                'cart_items': cart_items,
-                'total': total,
-                'tax': tax,
-                'grand_total': grand_total,
+                "order": order,
+                "cart_items": cart_items,
+                "total": total,
+                "tax": tax,
+                "grand_total": grand_total,
             }
 
-            return render(request, 'orders/payments.html', context)
+            return render(request, "orders/payments.html", context)
 
     else:
-        return redirect('checkout')
-
+        return redirect("checkout")
 
 
 def order_complete(request):
-    order_number = request.GET.get('order_number')
-    transID = request.GET.get('payment_id')
+    order_number = request.GET.get("order_number")
+    transID = request.GET.get("payment_id")
 
     try:
         order = Order.objects.get(order_number=order_number, is_ordered=True)
@@ -147,20 +160,20 @@ def order_complete(request):
 
         subtotal = 0
         for i in ordered_products:
-            subtotal += i.product_price*i.quantity
+            subtotal += i.product_price * i.quantity
 
         payment = Payment.objects.get(payment_id=transID)
 
         context = {
-            'order': order,
-            'ordered_products': ordered_products,
-            'order_number': order.order_number,
-            'transID': payment.payment_id,
-            'payment': payment,
-            'subtotal': subtotal,
+            "order": order,
+            "ordered_products": ordered_products,
+            "order_number": order.order_number,
+            "transID": payment.payment_id,
+            "payment": payment,
+            "subtotal": subtotal,
         }
 
-        return render(request, 'orders/order_complete.html', context)
+        return render(request, "orders/order_complete.html", context)
 
-    except(Payment.DoesNotExist, Order.DoesNotExist):
-        return redirect('home')
+    except (Payment.DoesNotExist, Order.DoesNotExist):
+        return redirect("home")
