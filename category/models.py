9d1c8d69ba526deca9718a4b98258bf5aa4140c7
lorenx16 <lotola2003@gmail.com>
2024-11-26 18:05:55 +0100

    feat: añadido un frigorífico

--- category/models.py
+++ category/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 
 # Create your models here.
 class Category(models.Model):
-    category_name = models.CharField(max_length=20, unique=True)
+    category_name = models.CharField(max_length=30, unique=True)
     description = models.CharField(max_length=255, blank=True)
     slug =  models.CharField(max_length=100, unique=True)
     cat_image =  models.ImageField(upload_to='photos/categories', blank=True)
