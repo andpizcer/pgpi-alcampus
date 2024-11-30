541e8d28206bb552d69d982e8e153a4ea3b7e7f9
Francisco Avilés Carrera <fraavicar1@alum.us.es>
2024-11-30 23:02:47 +0100

    change: static settings

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -103,6 +103,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
 STATICFILES_DIRS = [BASE_DIR / "ecommerce/static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
